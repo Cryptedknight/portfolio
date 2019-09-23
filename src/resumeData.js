00d52bba61676262cd8da63f5db35bda76d497dd
Utkarsh Dwivedi <udiisc100@gmail.com>
2019-09-24 00:37:36 +0530

    Initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+    "imagebaseurl":"https://Cryptedknight.github.io/",
+    "name": "Utkarsh Dwivedi",
+    "role": "Web Developer and Data Scientist",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "Ever since I got introduced to programming, computers and computer technologies have fascinated me. It was almost magic, that I can make something of value by just sitting in front of my computer. It was this fascination that lead to my passion for software development. I am really passionate about software development, data analytics and problem-solving or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/utkarsh-dwivedi-845066153/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Cryptedknight",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at ABV - Indian Institute of Information Technology and Management, Gwalior, and pursuing my B.Tech from here. I am a self taught MERN stack Web Developer, currently exploring Machine Learning. I also love problem-solving and have good Data Structures and Algorithms knowledge. I believe that there is no shortcut to success, one must put all his efforts to achieve a level.",
+    "address":"India",
+    "website":"https://Cryptedknight.github.io",
+    "education":[
+      {
+        "UniversityName":"ABV - Indian Insitute of Information Technology And Management",
+        "specialization":"B.Tech in Computer Science And Engineering",
+        "MonthOfPassing":"June",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Rani Laxmi Bai Memorial Senior Secondary School, Sec-3 Vikas Nagar, Lucknow",
+        "specialization":"CBSE Class XII",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2016",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"NodeJs"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
